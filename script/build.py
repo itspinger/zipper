6b8ea9d2c4493f6f2d669a30e78b69a24299162d
Dimitrije Mijailovic <pinger.business@gmail.com>
2022-12-27 01:53:26 +0100

    [Script] Rename config to build json

--- script/build.py
+++ script/build.py
@@ -1,4 +1,9 @@
-import json
+import json, os
+
+# This function is used to set the spigot engine
+# And create the jar file in the specified folder
+def setSpigotEngine(version, settings):
+    print("Installing BuildTools")
 
 # Open the configuration file
 # And setup the server based on the engine
@@ -12,6 +17,13 @@ engine = data['engine']
 version = data['version']
 settings = data['settings']
 
+if (engine == "spigot"):
+    setSpigotEngine(version, settings)
+else:
+    print("Unknown engine. \nCheck the list of engines currently available and try again")
+
 # Once we are finished building everything
 # Close the file
 file.close()
+    
+    
