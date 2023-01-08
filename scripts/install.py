3f2a33b69aeb221573e297f6d811e481fb7f1d29
Dimitrije Mijailovic <pinger.business@gmail.com>
2023-01-08 22:14:32 +0100

    Add spigot server http validation

--- scripts/install.py
+++ scripts/install.py
@@ -1,6 +1,36 @@
 import json, os, subprocess, validation as vld
+import urllib
+from urllib.request import urlopen
 
 file = open('../install.json')
+
+# This method returns the java versions which can compile
+# The certain server version
+def getJavaVersions(version):
+    versions = []
+    try:
+        response = urlopen("https://hub.spigotmc.org/versions/%s.json" % version)
+        
+        # Load json
+        data = json.loads(response.read())
+
+        # Check if it has the javaVersions tag
+        # If not return java 8
+        if "javaVersions" not in data:
+            return [8]
+
+        jvs = data["javaVersions"]
+        for i in range(jvs[0], jvs[1] + 1):
+            ujv = int(i) - 44
+
+            # Use regular list and check if not in the list
+            if ujv not in versions:
+                versions.append(ujv)
+               
+        return versions
+    except urllib.error.HTTPError as e:
+        print("Failed to grab data for version %s, code %d" % (version, e.code))
+        exit(-1)
 
 # Read the json data
 data = json.load(file)
@@ -20,13 +50,13 @@ else:
 # Check if install.sh file for downloading build tools exists...
 print("Checking for the install.sh file")
 dataFolder = os.path.join(os.getcwd(), "data")
-buildTools = os.path.join(dataFolder, "build.sh") # Change this to install.sh
+buildTools = os.path.join(dataFolder, "install.sh")
 
 if not os.path.exists(buildTools):
     print("Couldn't find the install.sh file, do `git pull` in your cmd to download it again.")
     exit()
 else:
-    print("install.sh file exists, we're good to go.")
+    print("Found the install.sh file inside the data folder, continuing..")
 
 # Close the file to quit the process
 file.close()
